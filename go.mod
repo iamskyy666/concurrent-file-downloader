b6f6d5117bdd13e44d4efe7e411fac67da5b8adb
Soumadip <39thdecember@gmail.com>
2026-05-04 15:26:15 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iamskyy666/concurrent-file-downloader
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// concurrent-file-downloader
+
+func main() {
+	fmt.Println("Hi")
+}
